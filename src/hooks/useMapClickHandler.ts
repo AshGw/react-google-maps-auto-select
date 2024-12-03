9263399c36a318544233f7fd68094bdef5cafb14
AshGw <ashrefgw@proton.me>
2024-12-03 21:28:47 +0100

    chore: handle errors

--- src/hooks/useMapClickHandler.ts
+++ src/hooks/useMapClickHandler.ts
@@ -11,10 +11,12 @@ export function useMapClickHandler({
   googleMap,
   onClickZoomLevel,
   setCurrentLocation,
+  handleAutoCompletedLocationError,
 }: {
   onClickZoomLevel?: number;
   googleMap: Optional<google.maps.Map>;
   setCurrentLocation: Dispatch<SetStateAction<Location>>;
+  handleAutoCompletedLocationError?: (error: Error) => void;
 }) {
   const onMapClick = useCallback(
     async (mapMouseEvent: google.maps.MapMouseEvent) => {
@@ -26,13 +28,24 @@ export function useMapClickHandler({
 
       googleMap?.panTo({ lat, lng });
       googleMap?.setZoom(onClickZoomLevel ?? 18);
-      const autoCompletedLocation = await getAutoCompletedLocationFromLatLng({
-        location: { lat, lng },
-      });
+      try {
+        const autoCompletedLocation = await getAutoCompletedLocationFromLatLng({
+          location: { lat, lng },
+        });
 
-      setCurrentLocation(autoCompletedLocation);
+        setCurrentLocation(autoCompletedLocation);
+      } catch (e) {
+        if (handleAutoCompletedLocationError) {
+          handleAutoCompletedLocationError(e as unknown as Error);
+        }
+      }
     },
-    [googleMap, setCurrentLocation, onClickZoomLevel]
+    [
+      googleMap,
+      setCurrentLocation,
+      onClickZoomLevel,
+      handleAutoCompletedLocationError,
+    ]
   );
 
   return { onMapClick };
