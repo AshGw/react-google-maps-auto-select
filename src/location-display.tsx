780dfe07f2710ee7c13e8e784e45c23017465c36
AshGw <ashrefgw@proton.me>
2024-12-03 20:11:02 +0100

    feat: add `PolymorphicRef`

--- src/location-display.tsx
+++ src/location-display.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useCallback, useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import {
   GoogleMap,
   Marker,
@@ -6,7 +6,7 @@ import {
   type Libraries,
 } from '@react-google-maps/api';
 import { Location } from './types';
-import { Optional } from 'ts-roids';
+import { Optional, Keys, EmptyObject } from 'ts-roids';
 import { usePanTo } from 'hooks';
 
 // Predeclared here for performance optimizations:
@@ -14,78 +14,92 @@ import { usePanTo } from 'hooks';
 // Declaring it as a constant ensures reusability and prevents redundant allocations.
 const GOOGLE_MAPS_LIBRARIES: Libraries = ['places'] as const;
 
-interface GoogleMapsLocationDisplayProps
-  extends React.HTMLAttributes<HTMLDivElement> {
+type PolymorphicRef<C extends React.ElementType> =
+  React.ComponentPropsWithRef<C>['ref'];
+
+type PolymorphicComponentProps<
+  C extends React.ElementType,
+  Props = EmptyObject,
+> = Props &
+  Omit<React.ComponentPropsWithoutRef<C>, Keys<Props>> & {
+    as?: C;
+  };
+
+interface GoogleMapsLocationDisplayOwnProps {
   initialLocation: Location;
+  onChangeLocation: (location: Location) => void;
   googleMapsApiKey: string;
   fallback?: React.ReactNode;
-  onChangeLocation: (location: Location) => void;
 }
 
-export const GoogleMapsLocationDisplay: React.FC<GoogleMapsLocationDisplayProps> =
-  React.forwardRef<HTMLDivElement, GoogleMapsLocationDisplayProps>(
-    (
-      {
-        initialLocation,
-        onChangeLocation,
-        googleMapsApiKey,
-        fallback,
-        ...props
-      },
-      ref
-    ) => {
-      const [googleMap, setGoogleMap] =
-        useState<Optional<google.maps.Map>>(null);
-      const [currentLocation, setCurrentLocation] =
-        useState<Location>(initialLocation);
+type GoogleMapsLocationDisplayProps<C extends React.ElementType> =
+  PolymorphicComponentProps<C, GoogleMapsLocationDisplayOwnProps>;
 
-      const { panTo } = usePanTo(googleMap);
+export const GoogleMapsLocationDisplay = React.forwardRef(
+  <C extends React.ElementType>(
+    {
+      initialLocation,
+      onChangeLocation,
+      googleMapsApiKey,
+      fallback = null,
+      as,
+      ...props
+    }: GoogleMapsLocationDisplayProps<C>,
+    ref: PolymorphicRef<C>
+  ) => {
+    const [googleMap, setGoogleMap] = useState<Optional<google.maps.Map>>(null);
+    const [currentLocation, setCurrentLocation] =
+      useState<Location>(initialLocation);
 
-      useEffect(() => {
-        panTo({
-          lat: initialLocation.lat,
-          lng: initialLocation.lng,
-        });
-      }, [initialLocation.lat, initialLocation.lng, panTo]);
+    const { panTo } = usePanTo(googleMap);
 
-      function onClickAnywhereOnMap(mapMouseEvent: google.maps.MapMouseEvent) {
-        const latLng = mapMouseEvent.latLng;
-        if (!latLng) return;
-
-        const lat = latLng.lat();
-        const lng = latLng.lng();
-        googleMap?.panTo({ lat, lng });
-        googleMap?.setZoom(18);
-      }
-
-      const { isLoaded } = useLoadScript({
-        googleMapsApiKey,
-        libraries: GOOGLE_MAPS_LIBRARIES,
+    useEffect(() => {
+      panTo({
+        lat: initialLocation.lat,
+        lng: initialLocation.lng,
       });
+    }, [initialLocation.lat, initialLocation.lng, panTo]);
 
-      if (!isLoaded && !fallback) {
-        return null;
-      }
+    function onClickAnywhereOnMap(mapMouseEvent: google.maps.MapMouseEvent) {
+      const latLng = mapMouseEvent.latLng;
+      if (!latLng) return;
 
-      return (
-        <div ref={ref} {...props}>
-          <Suspense fallback={fallback}>
-            <GoogleMap
-              zoom={10}
-              center={{
-                lat: currentLocation.lat,
-                lng: currentLocation.lng,
-              }}
-              mapContainerClassName="w-full rounded-md overflow-hidden h-[350px] shadow"
-              onLoad={(map) => setGoogleMap(map)}
-              onClick={onClickAnywhereOnMap}
-            >
-              <Marker position={currentLocation} />
-            </GoogleMap>
-          </Suspense>
-        </div>
-      );
+      const lat = latLng.lat();
+      const lng = latLng.lng();
+      googleMap?.panTo({ lat, lng });
+      googleMap?.setZoom(18);
     }
-  );
+
+    const { isLoaded } = useLoadScript({
+      googleMapsApiKey,
+      libraries: GOOGLE_MAPS_LIBRARIES,
+    });
+
+    const Component = as || React.Fragment;
+
+    if (!isLoaded && !fallback) {
+      return null;
+    }
+
+    return (
+      <Component ref={ref} {...props}>
+        <Suspense fallback={fallback}>
+          <GoogleMap
+            zoom={10}
+            center={{
+              lat: currentLocation.lat,
+              lng: currentLocation.lng,
+            }}
+            mapContainerClassName="w-full rounded-md overflow-hidden h-[350px] shadow"
+            onLoad={(map) => setGoogleMap(map)}
+            onClick={onClickAnywhereOnMap}
+          >
+            <Marker position={currentLocation} />
+          </GoogleMap>
+        </Suspense>
+      </Component>
+    );
+  }
+);
 
 GoogleMapsLocationDisplay.displayName = 'GoogleMapsLocationDisplay';
