b8d49e0cb4cc0e45332a200131762855caca618b
AshGw <ashrefgw@proton.me>
2024-12-03 19:02:54 +0100

    chore: add perf libs

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import {
   type Libraries,
 } from '@react-google-maps/api';
 
+// declared here for performance optimizations
+const GOOGLE_MAPS_LIBRARIES: Libraries = ['places'] as const;
+
 export const GoogleMapsLocationDisplay: React.FC<{}> = () => {
   return <></>;
 };
