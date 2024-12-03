c56bb6da1dadeeece7b2993afa8e27f41eaf64f5
AshGw <ashrefgw@proton.me>
2024-12-03 19:11:46 +0100

    chore: add libraries perf comment

---
+++ src/location-display.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  GoogleMap,
+  Marker,
+  useLoadScript,
+  type Libraries,
+} from '@react-google-maps/api';
+
+import { Location } from './types';
+
+// Predeclared here for performance optimizations:
+// Only the 'places' library is loaded to minimize payload size and enhance API loading speed.
+// Declaring it as a constant ensures reusability and prevents redundant allocations.
+const GOOGLE_MAPS_LIBRARIES: Libraries = ['places'] as const;
+
+interface GoogleMapsLocationDisplayProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  initialLocation: Location;
+  onChangeLocation: (location: Location) => void;
+}
+
+export const GoogleMapsLocationDisplay: React.FC<
+  GoogleMapsLocationDisplayProps
+> = () => {
+  return <></>;
+};
